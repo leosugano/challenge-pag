500754eba8a9e6d5295eccc19d3f3a94a883c372
Leo Sugano <leo_st29@hotmail.com>
2021-03-08 19:50:44 -0300

    Separate components in folders

---
+++ challenge-pag/challenge-pag/app/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  challenge-pag
+//
+//  Created by Leonardo Sugano on 08/03/21.
+//
+
+import UIKit
+import CoreData
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+ 
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
